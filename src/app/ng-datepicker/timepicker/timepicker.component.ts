18a597d9362544f9763a25d865d028a5b2529a85
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2022-08-31 23:01:50 +0300

    add interface to select hours

--- src/app/ng-datepicker/timepicker/timepicker.component.ts
+++ src/app/ng-datepicker/timepicker/timepicker.component.ts
@@ -13,4 +13,53 @@ export class TimepickerComponent {
   @Output() decrementHours = new EventEmitter();
   @Output() incrementMinutes = new EventEmitter();
   @Output() decrementMinutes = new EventEmitter();
+
+  hoursRange: string[] = [];
+  minutesRange: string[] = [];
+
+  timerVisible = true;
+  selectHourVisible = false;
+  selectMinuteVisible = false;
+
+  showHourRangeView(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.hoursRange = this.getHoursRange();
+
+    this.timerVisible = false;
+    this.selectMinuteVisible = false;
+    this.selectHourVisible = true;
+  }
+
+  showMinuteRangView(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.minutesRange = this.getMinutesRange();
+  }
+
+  getHoursRange(): string[] {
+    const result = [];
+
+    for (let i = 0; i < 24; i++) {
+      result.push(this.fixTimeZero(i));
+    }
+
+    return result;
+  }
+
+  getMinutesRange(): string[] {
+    const result = [];
+
+    for (let i = 0; i < 60; i + 5) {
+      result.push(this.fixTimeZero(i));
+    }
+
+    return result;
+  }
+
+  private fixTimeZero(hours: number): string {
+    return hours <= 0 || hours <= 9 ? `0${hours}` : `${hours}`;
+  }
 }
